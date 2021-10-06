0c1f608bb38fd3382a5653d7a7ec27dbdf4d7796
Anubhav Paras <anubhavparas94@gmail.com>
2021-10-06 15:12:09 -0400

    added doxygen files

---
+++ docs/doxygen/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['detect',['detect',['../classDetector.html#ae0c0a72fef22d6521e82c18424a4dd04',1,'Detector']]],
+  ['detector',['Detector',['../classDetector.html',1,'Detector'],['../classDetector.html#aa43b3d7bbd4b225855b4bcc93f70b225',1,'Detector::Detector()']]]
+];
