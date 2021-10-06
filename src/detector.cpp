8970a3c902c058e5b16abfdef5151e33548bcb03
Anubhav Paras <anubhavparas94@gmail.com>
2021-10-06 06:59:57 -0400

    added template code for detector

---
+++ src/detector.cpp
@@ -0,0 +1,13 @@
+/** Copyright 2021 Sakshi Kakde, Siddharth Telang, Anubhav Paras */
+
+#include <detector.hpp>
+#include <iostream>
+
+Detector::Detector() {}
+
+Detector::~Detector() {}
+
+void Detector::detect() {
+    std::cout << "Detecting objects" << std::endl;
+}
+
