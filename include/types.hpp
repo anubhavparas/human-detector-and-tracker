8bc260e78baec8500156b3165f5f68e3f6c25aa7
Anubhav Paras <anubhavparas94@gmail.com>
2021-10-18 04:02:24 -0400

    declared common datatypes being used in the code

---
+++ include/types.hpp
@@ -0,0 +1,21 @@
+/** Copyright 2021 Sakshi Kakde, Siddharth Telang, Anubhav Paras */
+
+#ifndef INCLUDE_TYPES_HPP_
+#define INCLUDE_TYPES_HPP_
+
+#include <utility>
+#include <vector>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <data.hpp>
+
+typedef std::vector<cv::Rect> Rectangles;
+typedef cv::InputArray Image;
+typedef cv::HOGDescriptor SVMHOGModel;
+typedef std::pair<Rectangles, std::vector<double>> RectanglesAndScores;
+typedef Data<RectanglesAndScores> DetectionOutput;
+typedef cv::Point3d Coord3D;
+typedef cv::Point Coord2D;
+
+#endif  // INCLUDE_TYPES_HPP_
