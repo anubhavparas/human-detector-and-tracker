cec72a08144ca6261b7ce109d7d25de8eae3d748
Anubhav Paras <anubhavparas94@gmail.com>
2021-10-18 04:04:53 -0400

    modified the detector class - added concrete HumanDetector declaration

--- include/detector.hpp
+++ include/detector.hpp
@@ -3,29 +3,92 @@
 #ifndef INCLUDE_DETECTOR_HPP_
 #define INCLUDE_DETECTOR_HPP_
 
+#include <memory>
+#include <vector>
+#include <model.hpp>
+#include <frame_transformation.hpp>
+#include <types.hpp>
+
 /**
  * @brief Generic object detection class
  * 
  */
+
 class Detector {
  public:
-  /**
-   * @brief Construct a new Detector object
-   * 
-   */
-  Detector();
-
-  /**
-   * @brief Destroy the Detector object
-   * 
-   */
-  ~Detector();
-
   /**
    * @brief detects objects in image
    * 
    */
-  void detect();
+  virtual std::vector<Coord3D> detect(const cv::Mat &inputData) = 0;
+  virtual ~Detector() {}
+};
+
+class HumanDetector : public Detector {
+ public:
+  /**
+   * @brief Construct a new Human Detector object
+   * 
+   */
+  HumanDetector();
+
+  /**
+   * @brief Construct a new Human Detector object based on some model
+   * 
+   * @param model model to be used be the detector to predict
+   */
+  HumanDetector(Model<DetectionOutput, Image>* model,
+                  FrameTransformation* robotFrame);
+
+  /**
+   * @brief Destroy the Human Detector object
+   * 
+   */
+  ~HumanDetector();
+
+  /**
+   * @brief detects the humans in a given image data
+   * displays the bounding boxes and the ids of all the humans detected
+   * 
+   * @param inputData input image data 
+   * @return std::vector<Coord3D> coordinates of all the detected humans
+   * in robot frame
+   */
+  std::vector<Coord3D> detect(const cv::Mat &inputData) override;
+
+ private:
+  // model to find the humans
+  Model<DetectionOutput, Image>* model;
+  // std::unique_ptr<Model<DetectionOutput, Image>> model;
+
+  // to get the coordinates in robot frame
+  FrameTransformation* robotFrame;
+  // std::unique_ptr<FrameTransformation> robotFrame;
+
+  /**
+   * @brief Get the list of centroid coordinates of the detected humans in an image
+   * 
+   * @param boundingBoxes list of bounding boxes of the humans detected
+   * @return std::vector<Coord2D> list of centroid (x,y) coordinates
+   */
+  std::vector<Coord2D> getCentroids(const Rectangles& boundingBoxes);
+
+  /**
+   * @brief Get the Robot Frame Coordinates
+   * 
+   * @param boundingBoxes list of bounding boxes of the humans detected
+   * @return std::vector<Coord3D> list of (x,y,z) coordinates in the robot frame
+   */
+  std::vector<Coord3D> getRobotFrameCoordinates(const Rectangles&);
+
+  /**
+   * @brief display the output with bounding boxes, confidence scores and id
+   * 
+   * @param inputData image data 
+   * @param predictionOutput pair of bounding boxes and respective scores
+   */
+  void displayOutput(const cv::Mat &inputData,
+                     const DetectionOutput &predictionOutput);
 };
 
 #endif  // INCLUDE_DETECTOR_HPP_
