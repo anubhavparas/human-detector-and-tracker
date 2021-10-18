e1dd0d7f7f72cb5b0372336e6769856ab1aa0bd0
Anubhav Paras <anubhavparas94@gmail.com>
2021-10-18 13:13:04 -0400

    another test to cover the defensive check in the resize method of Preprocessor class

--- test/test_preprocessor.cpp
+++ test/test_preprocessor.cpp
@@ -21,3 +21,15 @@ TEST(preprocessor_test, image_resize) {
     EXPECT_TRUE(image.size().width != originalSize.width);
     EXPECT_EQ(image.size().width, expectedSize.width);
 }
+
+TEST(preprocessor_test, image_resize_no_resize) {
+    std::string test_path = "../data/testdata/FudanPed00028.png";
+
+    PreProcessor preProcessor;
+    cv::Mat image = cv::imread(test_path);
+    cv::Size originalSize = image.size();
+    preProcessor.resize(image, image, cv::Size());
+
+    EXPECT_EQ(image.size().height,  originalSize.height);
+    EXPECT_EQ(image.size().width, originalSize.width);
+}
