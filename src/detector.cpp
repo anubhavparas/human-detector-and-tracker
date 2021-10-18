6087b7a9d815c8fbc3489542eaeba9270cb1e44a
Anubhav Paras <anubhavparas94@gmail.com>
2021-10-18 04:14:41 -0400

    added stubs for Detector and HumanDetector class

--- src/detector.cpp
+++ src/detector.cpp
@@ -2,12 +2,56 @@
 
 #include <detector.hpp>
 #include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
 
-Detector::Detector() {}
+typedef HumanDetector HD;
 
-Detector::~Detector() {}
+HD::HumanDetector() {}
 
-void Detector::detect() {
-    std::cout << "Detecting objects" << std::endl;
+
+HD::HumanDetector(Model<DetectionOutput, Image>* model,
+                  FrameTransformation* robotFrame) {
+  this->model = model;
+  this->robotFrame = robotFrame;
 }
+
+
+HD::~HumanDetector() {
+  if (this->model != nullptr) {
+    delete this->model;
+    this->model = nullptr;
+  }
+  if (this->robotFrame != nullptr) {
+    delete this->robotFrame;
+    this->robotFrame = nullptr;
+  }
+}
+
+std::vector<Coord2D> HD::getCentroids(const Rectangles& boundingBoxes) {
+  std::vector<Coord2D> centroids;
+  
+  return centroids;
+}
+
+std::vector<Coord3D> HD::getRobotFrameCoordinates(
+                                const Rectangles& boundingBoxes) {
+  
+  std::vector<Coord3D> robotFrameCoordinates;
+  
+  return robotFrameCoordinates;
+}
+
+void HD::displayOutput(const cv::Mat &image,
+                       const DetectionOutput &predictionOutput) {
+  
+}
+
+std::vector<Coord3D> HD::detect(const cv::Mat &inputData) {
+  std::cout << "Detecting objects" << std::endl;
+  
+  std::vector<Coord3D> coordinates;
+  return coordinates;
+}
+
 
