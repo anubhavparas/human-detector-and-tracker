54539e5700087c008668df3a56aadcc560fced1f
Anubhav Paras <anubhavparas94@gmail.com>
2021-10-18 04:17:38 -0400

    added stubs for PreProcessor class

---
+++ src/preprocessor.cpp
@@ -0,0 +1,12 @@
+/** Copyright 2021 Sakshi Kakde, Siddharth Telang, Anubhav Paras */
+
+#include <preprocessor.hpp>
+
+PreProcessor::PreProcessor() {}
+
+PreProcessor::~PreProcessor() {}
+
+void PreProcessor::resize(const cv::Mat& input,
+                            const cv::Mat& output, cv::Size size) {
+    
+}
