97b82647d8fb5e751378421e336d8e596a3d0b0e
Anubhav Paras <anubhavparas94@gmail.com>
2021-10-18 04:11:25 -0400

    added stubs for DataReader - ImageReader

---
+++ src/datareader.cpp
@@ -0,0 +1,19 @@
+/** Copyright 2021 Sakshi Kakde, Siddharth Telang, Anubhav Paras */
+
+#include <datareader.hpp>
+
+// ImageReader
+ImageReader::ImageReader() {}
+
+ImageReader::~ImageReader() {}
+
+cv::Mat ImageReader::readData(std::string path) {
+  cv::Mat frame;
+  return frame;
+}
+
+
+
+
+
+template class DataReader<cv::Mat>;
