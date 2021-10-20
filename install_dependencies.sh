6979540998cefe562f612706bc2b9c6c751eb0c7
Sakshi Kakde <kakdesakshi@gmail.com>
2021-10-20 02:52:10 -0400

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,6 +4,7 @@ sudo apt-get install -y -qq lcov
 # OpenCV install
 sudo apt-get update -y
 sudo apt-get install -y libopencv-dev
+sudo apt-get install -y libopencv-contrib-dev
 # sudo apt-get install -y build-essential
 # sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 # sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
