f67c775f55e74905244902e8c8cbd05811e56b9c
sakshikakde <kakdesakshi@gmail.com>
2021-10-24 18:54:55 -0400

    [2.3.5]: Frame transformation implementation

--- include/frame_transformation.hpp
+++ include/frame_transformation.hpp
@@ -11,6 +11,11 @@ class FrameTransformation {
   FrameTransformation();
   virtual ~FrameTransformation();
   virtual Coord3D getRobotFrame(Coord2D imageCoordinates);
+ private:
+  Eigen::MatrixXf K;
+  Eigen::MatrixXf T_c;
+  Eigen::MatrixXf T_r;
+  Eigen::MatrixXf P;
 };
 
 #endif  // INCLUDE_FRAME_TRANSFORMATION_HPP_
--- src/frame_transformation.cpp
+++ src/frame_transformation.cpp
@@ -2,12 +2,33 @@
 
 #include <frame_transformation.hpp>
 
-FrameTransformation::FrameTransformation() {}
+FrameTransformation::FrameTransformation() {
+    // camera intrinsic matrix
+    this->K = Eigen::MatrixXf(3, 3);
+    this->K << 964.828979, 0, 643.788025, 0, 964.828979, 484.407990, 0, 0, 1;
+    this->T_c = Eigen::MatrixXf(3, 4);
+    this->T_c << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0;
+    this->T_r = Eigen::MatrixXf(4, 4);;
+    this->T_r<< 0, 0, 1, 10, -1, 0, 0, 10, 0, -1, 0, 10, 0, 0, 0, 1;
+    // Projection matrix
+    this->P = this->K * this->T_c * this->T_r;
+}
 
 FrameTransformation::~FrameTransformation() {}
 
 Coord3D FrameTransformation::getRobotFrame(Coord2D imageCoordinates) {
   // logic goes here
-  imageCoordinates.x = imageCoordinates.x;
-  return Coord3D(0, 0, 0);
+  // Calculate P inverse
+  Eigen::MatrixXf P_T = this->P.transpose();
+  Eigen::MatrixXf P_inv = P_T * (this->P * P_T).inverse();
+  // Image coordinates to homogenous coordinates
+  Eigen::MatrixXf x(3, 1);
+  x << imageCoordinates.x, imageCoordinates.y, 1;
+  Eigen::MatrixXf X = P_inv * x;
+  // populate Coord3D
+  Coord3D X_robot;
+  X_robot.x = X(0, 0) / X(3, 0);
+  X_robot.y = X(1, 0) / X(3, 0);
+  X_robot.z = X(2, 0) / X(3, 0);
+  return X_robot;
 }
