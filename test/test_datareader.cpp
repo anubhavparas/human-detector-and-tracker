1486f0fc7f5b9cbe06c22a95b1e678dc54b06e47
Anubhav Paras <anubhavparas94@gmail.com>
2021-10-25 00:56:02 -0400

    [2.9]: path to test data changed

--- test/test_datareader.cpp
+++ test/test_datareader.cpp
@@ -7,7 +7,8 @@
 
 TEST(image_reader_test, image_read_success) {
     DataReader<cv::Mat> *imageReader = new ImageReader();
-    std::string test_path = "../data/testdata/FudanPed00028.png";
+    std::string test_path =
+            "../data/testdata/unit_test_data/pos/FudanPed00028.png";
 
     cv::Mat image = imageReader->readData(test_path);
     EXPECT_FALSE(image.empty());
