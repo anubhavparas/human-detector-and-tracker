13f1d37d957be2451453a1706fadf562021c884b
Anubhav Paras <anubhavparas94@gmail.com>
2021-10-25 00:53:48 -0400

    [2.9]: executable to run the model evaluation test

---
+++ app/evaluate.cpp
@@ -0,0 +1,47 @@
+/** Copyright 2021 Sakshi Kakde, Siddharth Telang, Anubhav Paras */
+
+#include <unordered_set>
+#include <iostream>
+#include <memory>
+#include <opencv2/opencv.hpp>
+
+#include <driver.hpp>
+#include <datareader.hpp>
+#include <preprocessor.hpp>
+#include <model.hpp>
+#include <detector.hpp>
+#include <frame_transformation.hpp>
+#include <types.hpp>
+
+int main() {
+  std::cout << "Welcome to Human Detector and Tracker."
+            << std::endl;
+
+  std::unique_ptr<DataReader<cv::Mat>> dataReader(new ImageReader());
+  std::unique_ptr<PreProcessor> preProcessor(new PreProcessor());
+  std::unique_ptr<Model<DetectionOutput, Image>> svmModel(
+                                            new SVMHumanClassifier());
+  std::unique_ptr<FrameTransformation> robotFrame(new FrameTransformation());
+
+
+  std::unique_ptr<Detector> humanDetector(new HumanDetector(std::move(svmModel),
+                                              std::move(robotFrame)));
+
+  Driver* detectionDriver = new Driver(std::move(dataReader),
+                                       std::move(preProcessor),
+                                       std::move(humanDetector),
+                                       false);
+
+  std::string test_dir = "../data/testdata";
+  double average_error = detectionDriver->evaluateModel(test_dir);
+  std::cout << "The average error of the model for the dataset:: "
+            << average_error
+            << std::endl;
+
+  delete detectionDriver;
+
+  cv::destroyAllWindows();
+  return 0;
+}
+
+
